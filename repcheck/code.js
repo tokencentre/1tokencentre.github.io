396bee2badd16b705a8338dd673e44d45f51023b
etherdexproject <etherdexproject@noreply.github.com>
2019-02-10 19:09:29 +0300

    Update code.js

--- repcheck/code.js
+++ repcheck/code.js
@@ -2,9 +2,9 @@
 
 function checker() { // eslint-disable-line
   const name = $('#name')[0].value.replace(/[^A-Za-z0-9_]/gi, '');
-  const validNames = ['EtherDeltaZack_twitter', 'EtherDeltaRep1_twitter', 'EtherDeltaRep2_twitter', 'EtherDeltaRep3_twitter', 'EtherDeltaRep4_twitter', 'EtherDeltaUX_twitter'];
+  const validNames = ['Token_Centre''];
   if (validNames.map(x => x.toUpperCase()).indexOf(name.toUpperCase()) >= 0) {
-    $('#result').html(`${name.toUpperCase()} is an official EtherDelta representative.`);
+    $('#result').html(`${name.toUpperCase()} is an official TokenCentre representative.`);
   } else {
     $('#result').html(`${name.toUpperCase()} is NOT OFFICIAL.`);
   }
